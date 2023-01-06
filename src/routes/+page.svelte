78a71f7be430be2f2272372f9c0f1b1ef56089bb
spences10 <spences10apps@gmail.com>
2023-01-06 19:29:55 +0000

    feat: :lipstick: add gradient styles to index

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,17 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="flex flex-col items-center justify-center h-screen">
+	<h1
+		class="bg-clip-text text-transparent bg-gradient-to-tr from-primary to-secondary via-accent animate-gradient-xy font-extrabold text-6xl md:text-9xl py-8"
+	>
+		Tailwind Gradient Text
+	</h1>
+	<p
+		class="bg-clip-text text-transparent bg-gradient-to-br from-primary to-secondary via-accent animate-gradient-y font-extrabold text-3xl md:text-6xl py-8"
+	>
+		This animation is on the vertical axis
+	</p>
+	<p
+		class="bg-clip-text text-transparent bg-gradient-to-tl from-primary to-secondary via-accent animate-gradient-x font-extrabold text-3xl md:text-6xl py-8"
+	>
+		This animation is on the horizontal axis
+	</p>
+</div>
